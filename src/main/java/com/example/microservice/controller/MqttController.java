20966a63837b84c1482ed4958b5e478884bb40bd
mzoneboy <young_xiao@yeah.net>
2020-08-20 13:24:06 +0800

    new functions: mqtt

---
+++ src/main/java/com/example/microservice/controller/MqttController.java
@@ -0,0 +1,35 @@
+package com.example.microservice.controller;
+
+import com.example.microservice.iot.MqttPublisher;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("mqtt")
+@RestController
+public class MqttController {
+    private static final Logger logger = LoggerFactory.getLogger(MqttController.class);
+    @RequestMapping("pub")
+    public String pubMsg(String topic, String msg){
+        MqttPublisher mqttPublisher;
+        try {
+            mqttPublisher = new MqttPublisher();
+            MqttMessage mqttMessage = new MqttMessage();
+            // Quality of Service，QoS
+            // •级别0：尽力而为。消息可能会丢，但绝不会重复传输
+            // •级别1：消息绝不会丢，但可能会重复传输
+            // •级别2：恰好一次。每条消息肯定会被传输一次且仅传输一次
+            mqttMessage.setQos(1);
+            mqttMessage.setRetained(true);
+            mqttMessage.setPayload(msg.getBytes());
+            mqttPublisher.publish(topic, mqttMessage);
+        } catch (MqttException e) {
+            logger.error(e.toString());
+        }
+
+        return "success";
+    }
+}
---
+++ src/main/java/com/example/microservice/iot/PushCallback.java
@@ -0,0 +1,28 @@
+package com.example.microservice.iot;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PushCallback implements MqttCallback {
+    private static final Logger logger = LoggerFactory.getLogger(PushCallback.class);
+    @Override
+    public void connectionLost(Throwable throwable) {
+        logger.info("连接丢失.............");
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        logger.info("接收消息主题 : " + topic);
+        logger.info("接收消息Qos : " + mqttMessage.getQos());
+        logger.info("接收消息内容 : " + new String(mqttMessage.getPayload()));
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+        logger.info("deliveryComplete.............");
+    }
+}
